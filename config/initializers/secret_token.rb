c1c204577ee696bc06545d083ecdc1b525040572
Rich Daley <rich@richd.me>
2014-11-08 12:39:50 +0000

    Completely blank Rails app with Vagrant setup from https://gorails.com/guides/using-vagrant-for-rails-development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nage::Application.config.secret_key_base = '07b54ec53731f7b0a683ebdfb379e55af697a149fb822cb9e41fe9a40525154db86a944c7cadad3cd59cbd1ba8444a958529cb81b794da19bb25a110d87da088'
